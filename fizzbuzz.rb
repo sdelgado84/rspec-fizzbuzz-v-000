aa91773f73abd7052ffdad813259f2f31b2be160
Salvador Delgado <sdelgado84@gmail.com>
2018-02-17 04:52:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz
-if fizz_3==fizzbuzz(3)
+if fizz_3 == fizzbuzz(3)
   puts "Fizz"
-elsif fizz_5==fizzbuzz(5)
+elsif fizz_5 == fizzbuzz(5)
   puts "buzz"
-elseif fizz_15==fizzbuzz(15)
+elseif fizz_15 == fizzbuzz(15)
 puts "FizzBuzz"
-elseif fizz_4==fizzbuzz(4)
+elseif fizz_4 == fizzbuzz(4)
 puts "nil"
   end
 end
