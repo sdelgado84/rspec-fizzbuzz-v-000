7739f6a38e687a775048438c59c67d848b92a150
Salvador Delgado <sdelgado84@gmail.com>
2018-02-17 05:13:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-if int % 3 == 3
+if int % 3 == 0
   return "Fizz"
-elsif int % 5 == 5
+elsif int % 5 == 0
   return "buzz"
-elsif int % 3 == 3 && int % 5 == 5
+elsif int % 3 == 0 && int % 5 == 0
 puts "FizzBuzz"
-elsif int % 4 == 4
+elsif int % 4 == 0
 puts "nil"
   end
 end
